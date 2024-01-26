2b2c5e44f67e6fb9f4a0a4af8402798dbb404a31
zangzelindocker12 <zangzelin>
2024-01-26 14:29:34 +0000

    auto push

--- baseline_dr.py
+++ baseline_dr.py
@@ -11,10 +11,10 @@ import plotly.graph_objects as go
 from sklearn.decomposition import PCA
 import pacmap
 import numpy as np
-from umap.parametric_umap import ParametricUMAP
+# from umap.parametric_umap import ParametricUMAP
 from sklearn.preprocessing import MinMaxScaler
 from ivis import Ivis
-import pytorch_lightning as pl
+# import pytorch_lightning as pl
 # import patemb_main_imagenet
 import uuid
 import argparse
